89184a542145b97707dc74d2b5ae0bc5b5c9c94f
Semishin <SemBegW@gmail.com>
2015-07-08 09:24:46 +0300

    ордера

--- application/classes/Controller/Admin/Order.php
+++ application/classes/Controller/Admin/Order.php
@@ -1,7 +1,6 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sema
- * Date: 08.07.15
- * Time: 9:13
- */ 
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Admin_Order extends Controller_Crud
+{
+    protected $_model = 'Order';
+}
