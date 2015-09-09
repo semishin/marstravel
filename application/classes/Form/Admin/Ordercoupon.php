3769d4d3a22276946b42377c78cf86a0a1df7683
v.sergej <v.sergej@ariol.by>
2015-09-09 12:48:20 +0300

    add css coupon succes order

--- application/classes/Form/Admin/Ordercoupon.php
+++ application/classes/Form/Admin/Ordercoupon.php
@@ -19,7 +19,6 @@ class Form_Admin_Ordercoupon extends CM_Form_Abstract
         $this->set_field('date', new CM_Field_String(), 3);
         $this->set_field('quantity_adults', new CM_Field_String(), 5);
         $this->set_field('quantity_children', new CM_Field_String(), 7);
-        $this->set_field('cost', new CM_Field_String(), 9);
         $this->set_field('fio', new CM_Field_String(), 11);
         $this->set_field('dob', new CM_Field_String(), 13);
         $this->set_field('passport', new CM_Field_String(), 15);
