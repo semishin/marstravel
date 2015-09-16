6954647f5dea424bb73285a75adfac1db3b4e7a5
info@ariol.by <info@ariol.by>
2015-09-16 09:41:58 +0200

    images

--- application/logs/2015/09/09.php
+++ application/logs/2015/09/09.php
@@ -326,3 +326,31 @@
 #10 /home/mars.ariol.by/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
 #11 /home/mars.ariol.by/index.php(140): Kohana_Request->execute()
 #12 {main} in /home/mars.ariol.by/modules/image/classes/Kohana/Image/GD.php:91
+2015-09-09 18:44:23 --- EMERGENCY: Kohana_Exception [ 0 ]: Not an image or invalid image:  ~ MODPATH/image/classes/Kohana/Image.php [ 107 ] in /home/mars.ariol.by/modules/image/classes/Kohana/Image/GD.php:91
+2015-09-09 18:44:23 --- DEBUG: #0 /home/mars.ariol.by/modules/image/classes/Kohana/Image/GD.php(91): Kohana_Image->__construct('/home/mars.ario...')
+#1 /home/mars.ariol.by/modules/image/classes/Kohana/Image.php(54): Kohana_Image_GD->__construct('/home/mars.ario...')
+#2 /home/mars.ariol.by/modules/ariol/classes/CM/Form/Abstract.php(175): Kohana_Image::factory('/home/mars.ario...')
+#3 /home/mars.ariol.by/modules/ariol/classes/CM/Form/Abstract.php(97): CM_Form_Abstract->after_submit()
+#4 /home/mars.ariol.by/modules/ariol/classes/Controller/Crud.php(216): CM_Form_Abstract->submit()
+#5 /home/mars.ariol.by/modules/ariol/classes/Controller/Crud.php(190): Controller_Crud->process_form(Object(Model_Hotel))
+#6 /home/mars.ariol.by/system/classes/Kohana/Controller.php(84): Controller_Crud->action_create()
+#7 [internal function]: Kohana_Controller->execute()
+#8 /home/mars.ariol.by/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Admin_Hotel))
+#9 /home/mars.ariol.by/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#10 /home/mars.ariol.by/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#11 /home/mars.ariol.by/index.php(140): Kohana_Request->execute()
+#12 {main} in /home/mars.ariol.by/modules/image/classes/Kohana/Image/GD.php:91
+2015-09-09 18:45:16 --- EMERGENCY: Kohana_Exception [ 0 ]: Not an image or invalid image:  ~ MODPATH/image/classes/Kohana/Image.php [ 107 ] in /home/mars.ariol.by/modules/image/classes/Kohana/Image/GD.php:91
+2015-09-09 18:45:16 --- DEBUG: #0 /home/mars.ariol.by/modules/image/classes/Kohana/Image/GD.php(91): Kohana_Image->__construct('/home/mars.ario...')
+#1 /home/mars.ariol.by/modules/image/classes/Kohana/Image.php(54): Kohana_Image_GD->__construct('/home/mars.ario...')
+#2 /home/mars.ariol.by/modules/ariol/classes/CM/Form/Abstract.php(175): Kohana_Image::factory('/home/mars.ario...')
+#3 /home/mars.ariol.by/modules/ariol/classes/CM/Form/Abstract.php(97): CM_Form_Abstract->after_submit()
+#4 /home/mars.ariol.by/modules/ariol/classes/Controller/Crud.php(216): CM_Form_Abstract->submit()
+#5 /home/mars.ariol.by/modules/ariol/classes/Controller/Crud.php(202): Controller_Crud->process_form(Object(Model_Hotel))
+#6 /home/mars.ariol.by/system/classes/Kohana/Controller.php(84): Controller_Crud->action_edit()
+#7 [internal function]: Kohana_Controller->execute()
+#8 /home/mars.ariol.by/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Admin_Hotel))
+#9 /home/mars.ariol.by/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#10 /home/mars.ariol.by/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#11 /home/mars.ariol.by/index.php(140): Kohana_Request->execute()
+#12 {main} in /home/mars.ariol.by/modules/image/classes/Kohana/Image/GD.php:91
